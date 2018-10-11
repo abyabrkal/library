5b3a3e32d8e55805e7a5b52bc113d49d1d76f192
Aby Abraham <abyabrkal@gmail.com>
2018-10-11 11:48:45 +0400

    Added Debug module (DEBUG=*) and Morgan for network traffic related messages.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var chalk = require('chalk');
+var debug = require('debug')('app');
+var morgan = require('morgan');
 
 var app = express();
+
+app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
     res.send('Hello from my Library');
