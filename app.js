c64b473102a85190f15151fdcf599a7db4616699
Aby Abraham <abyabrkal@gmail.com>
2018-10-11 15:10:59 +0400

    Tooling Essentials. NPM START scripts, ESLint, ES6, Nodemon config and ENV variables.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path = require('path');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
@@ -13,9 +14,9 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist'))
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.listen(3000, () => {
-    debug(`listening on port ${chalk.magenta('3000')}`);
+app.listen(port, () => {
+  debug(`listening on port ${chalk.magenta(port)}`);
 });
