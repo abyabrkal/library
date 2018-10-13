e83321e2dfeb283e4323d0bb9cc75c3893c49bbe
Aby Abraham <abyabrkal@gmail.com>
2018-10-13 12:32:50 +0400

    Album template ready for routing.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
@@ -16,8 +17,24 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+
+bookRouter.route('/books')
+  .get((req, res) => {
+    res.send('hello books');
+  });
+
+
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Library' });
+  res.render(
+    'index', 
+    { 
+      nav: [
+        { link: '/books', title: 'Books' }, 
+        { link: '/authors', title: 'Authors' }
+      ],
+      title: 'Library' 
+    }
+  );
 });
 
 app.listen(port, () => {
