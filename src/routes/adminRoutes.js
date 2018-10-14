f3a11fe289b017b5dc557fc0540a9da6908a2da3
Aby Abraham <abyabrkal@gmail.com>
2018-10-14 15:58:21 +0400

    MongoDB Setup book list added with id updated to use mongo's _id. But, the Single book view is broken now. Lets fix it.

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,12 +1,73 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+const debug = require('debug')('app:adminRoutes');
 const adminRouter = express.Router();
 
 
-function router(nav){
+const books = [
+    {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false,
+    },
+    {
+    title: 'Les Miserables',
+    genre: 'Historical Fiction',
+    author: 'Victor Hugo',
+    read: false,
+    },
+    {
+    title: 'Da Vinci Code',
+    genre: 'Suspense Thriller',
+    author: 'Dan Brown',
+    read: false,
+    },
+    {
+    title: 'Angels and Demons',
+    genre: 'Suspense Thriller',
+    author: 'Dan Brown',
+    read: false,
+    },
+    {
+    title: 'The Dark World',
+    genre: 'Fantasy',
+    author: 'Henry Kuttner',
+    read: false,
+    },
+    {
+    title: 'A Journey into the Centre of the Earth',
+    genre: 'Suspense Fiction',
+    author: 'Jules Verne',
+    read: false,
+    },
+];
 
+
+function router(nav){
     adminRouter.route('/')
         .get((req, res) => {
-            res.send('Inserting books');
+
+            const url = 'mongodb://localhost:27017';
+            const dbName = 'libraryApp';
+
+            (async function mongo(){
+                let client;
+                try {
+                    client = await MongoClient.connect(url);
+                    debug('Connected to server: success');
+
+                    const db = client.db(dbName);
+
+                    const response = await db.collection('books').insertMany(books)
+                    res.json(response);
+                } catch(err) {
+                    debug(err.stack);
+                }
+
+                client.close();
+            }())
+            //res.send('Inserting books');
         });
 
     return adminRouter;
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,57 +1,79 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+const debug = require('debug')('app:bookRoutes');
 const bookRouter = express.Router();
 
 function router(nav) {
-    
+
     const books = [
         {
-        title: 'War and Peace',
-        genre: 'Historical Fiction',
-        author: 'Lev Nikolayevich Tolstoy',
-        read: false,
+            title: 'War and Peace',
+            genre: 'Historical Fiction',
+            author: 'Lev Nikolayevich Tolstoy',
+            read: false,
         },
         {
-        title: 'Les Miserables',
-        genre: 'Historical Fiction',
-        author: 'Victor Hugo',
-        read: false,
+            title: 'Les Miserables',
+            genre: 'Historical Fiction',
+            author: 'Victor Hugo',
+            read: false,
         },
         {
-        title: 'Da Vinci Code',
-        genre: 'Suspense Thriller',
-        author: 'Dan Brown',
-        read: false,
+            title: 'Da Vinci Code',
+            genre: 'Suspense Thriller',
+            author: 'Dan Brown',
+            read: false,
         },
         {
-        title: 'Angels and Demons',
-        genre: 'Suspense Thriller',
-        author: 'Dan Brown',
-        read: false,
+            title: 'Angels and Demons',
+            genre: 'Suspense Thriller',
+            author: 'Dan Brown',
+            read: false,
         },
         {
-        title: 'The Dark World',
-        genre: 'Fantasy',
-        author: 'Henry Kuttner',
-        read: false,
+            title: 'The Dark World',
+            genre: 'Fantasy',
+            author: 'Henry Kuttner',
+            read: false,
         },
         {
-        title: 'A Journey into the Centre of the Earth',
-        genre: 'Suspense Fiction',
-        author: 'Jules Verne',
-        read: false,
+            title: 'A Journey into the Centre of the Earth',
+            genre: 'Suspense Fiction',
+            author: 'Jules Verne',
+            read: false,
         },
     ];
-    
+
     bookRouter.route('/')
         .get((req, res) => {
-        res.render(
-            'bookListView',
-            { 
-            nav,
-            title: 'Books' ,
-            books,
-            }
-        );
+            const url = 'mongodb://localhost:27017';
+            const dbName = 'libraryApp';
+
+            (async function mongo() {
+                let client;
+                try {
+                    client = await MongoClient.connect(url);
+                    debug('Connected to server: success');
+
+                    const db = client.db(dbName);
+
+                    const col = await db.collection('books');
+
+                    const books = await col.find().toArray();
+
+                    res.render(
+                        'bookListView',
+                        {
+                            nav,
+                            title: 'Books',
+                            books,
+                        }
+                    );
+                } catch (err) {
+                    debug(err.stack);
+                }
+                client.close();
+            }());
         });
 
 
@@ -64,7 +86,7 @@ function router(nav) {
                 'bookView',
                 {
                     nav,
-                    title: 'Books' ,
+                    title: 'Books',
                     book: books[id],
                 }
             );
