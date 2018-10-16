2283e53fcc2f41ea97bae8730217dfd2f4c2d82a
Aby Abraham <abyabrkal@gmail.com>
2018-10-16 15:14:31 +0400

    Passport and a Local Strategy added.

--- app.js
+++ app.js
@@ -4,13 +4,21 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ urlencoded: true }))
+app.use(bodyParser.urlencoded({ urlencoded: false }));
+app.use(cookieParser());
+app.use(session({ secret: 'library' }));
+
+require('./src/config/passport.js')(app);
+
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
