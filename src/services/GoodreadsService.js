1b64730aeafd22deca753082ebcf44013d4ba9a5
Aby Abraham <abyabrkal@gmail.com>
2018-10-18 10:09:08 +0400

    Goodreads API added. Worked with hard coded api function. Image needs to added in jumbotron book page

--- src/services/GoodreadsService.js
+++ src/services/GoodreadsService.js
@@ -1,8 +1,29 @@
+const axios = require('axios');
+const xml2js = require('xml2js');
+const debug = require('debug')('app:goodreadsService');
+
+// xml2js defaults to making arrays for even one item.
+const parser = xml2js.Parser({ explicitArray: false} );
 
 function goodreadsService(){
-    function getBookById(){
+    function getBookById(id) {
         return new Promise((resolve, reject) => {
-            resolve({ description: 'our description' })
+            axios.get('https://www.goodreads.com/book/show/656.xml?key=9aVudE6nAGbs4XUZMDnAA')
+                .then((response) => {
+                    parser.parseString(response.data, (err, result) => {
+                        if (err) {
+                            debug(err);
+                        } else {
+                            debug(result);
+                            resolve(result.GoodreadsResponse.book);
+                        }
+                    })
+                })
+                .catch((error) => {
+                    reject(error);
+                    debug(error);
+                })
+            
         });
     }
 
